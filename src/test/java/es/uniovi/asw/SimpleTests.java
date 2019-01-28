b88a55ed701cae2634616874cad8f8cb0d761eee
Jose Emilio Labra Gayo <jelabra@gmail.com>
2019-01-28 12:32:39 +0100

    Added simple tests

--- src/test/java/es/uniovi/asw/SimpleTests.java
+++ src/test/java/es/uniovi/asw/SimpleTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.util.NestedServletException;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -16,14 +17,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class SimpleTest {
+public class SimpleTests {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
-    public void exampleTest() throws Exception {
+    public void landingPageTest() throws Exception {
         this.mvc.perform(get("/")).andExpect(status().isOk());
     }
 
+    @Test
+    public void SearchTest() throws Exception {
+        this.mvc.perform(get("/search?name=foo")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void SortTest() throws Exception {
+        this.mvc.perform(get("/sort")).andExpect(status().isOk());
+    }
+
+    @Test(expected = NestedServletException.class)
+    public void SearchErrorTest() throws Exception {
+        this.mvc.perform(get("/search?name=error")).andExpect(status().isOk());
+    }
 }
